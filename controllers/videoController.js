d0ce2f1577c7373bfec1c1c8d042191fada807fd
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-05-21 16:06:10 +0900

    eslint

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async(req, res) => {
     try{
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({ _id: -1 });
         res.render("home", { pageTitle: "Home", videos });   
     } catch(error) {
         console.log(error);
@@ -74,6 +74,8 @@ export const deleteVideo = async(req, res) => {
     }
     try {
         await Video.findOneAndRemove({ _id: id });
-    } catch(error) {}
+    } catch(error) {
+        console.log(error);
+    }
     res.redirect(routes.home);
 };
