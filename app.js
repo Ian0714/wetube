2797b6389fea4326ba848c42b76dff26a3a8f171
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-12 15:48:36 +0900

    sessions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import session from "express-session";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -12,6 +13,7 @@ import { localsMiddleware } from "./middlewares";
 const app = express();
 
 import "./passport";
+import { TRUE } from "node-sass";
 
 app.use(helmet());
 app.set('view engine', "pug");
@@ -21,6 +23,11 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
