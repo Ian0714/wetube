462c9c6614d6f881f2e81063c533970dce1b15cb
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-17 14:22:56 +0900

    video recorder

---
+++ assets/js/videoRecorder.js
@@ -0,0 +1,28 @@
+const { initialize } = require("passport");
+
+const videoContainer = document.getElementById("jsRecordContainer");
+const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
+
+const startRecording = async() => {
+    try{
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: { width: 1280, height: 720 }
+        });
+    videoPreview.srcObject = stream;
+    videoPreview.muted = true;
+    videoPreview.play();
+    } catch(error) {
+        recordBtn.innerHTML = "Can't record";
+        recordBtn.removeEventListener("click", startRecording);
+    }
+}
+
+function init(){
+    recordBtn.addEventListener("click", startRecording);
+}
+
+if(videoContainer){
+    init();
+}
